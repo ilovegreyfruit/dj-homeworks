dd42ef7fa6e50a158a0f7ef3d897743c545e13d7
Nikita Schwarz <ilovegreyfruit@yandex.ru>
2025-04-08 19:07:14 +0300

    made 3.2 homework

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,25 @@
-from rest_framework import serializers
+from email.policy import default
+from itertools import product
 
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
-
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -23,7 +28,8 @@ class StockSerializer(serializers.ModelSerializer):
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
@@ -36,7 +42,15 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={
+                    'quantity': position['quantity'],
+                    'price': position['price'],
+                }
+            )
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
